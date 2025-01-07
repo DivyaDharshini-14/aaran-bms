fd2f4c708b939cdef6049baf92bb8ac309ca0759
Divya <divyadharshini146@gmail.com>
2025-01-07 18:30:20 +0530

    #4 create Job.md file

---
+++ aaran/Aadmin/Helper/Job.md
@@ -0,0 +1,3 @@
+## 1- Refactor -Divya
+- database
+- migrate
--- aaran/Common/routes.php
+++ aaran/Common/routes.php
@@ -5,22 +5,20 @@ use Illuminate\Support\Facades\Route;
 //Common
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 
-    Route::get('/commons/{id?}', App\Livewire\Common\CommonList::class)->name('commons');
-    Route::get('/labels', App\Livewire\Common\LabelList::class)->name('labels');
-
-    Route::get('/cities/{id}', App\Livewire\Common\Index::class)->name('cities');
-    Route::get('/states/{id}', App\Livewire\Common\Index::class)->name('states');
-    Route::get('/pin-codes/{id}', App\Livewire\Common\Index::class)->name('pin-codes');
-    Route::get('/countries/{id}', App\Livewire\Common\Index::class)->name('countries');
-    Route::get('/hsn-codes/{id}', App\Livewire\Common\Index::class)->name('hsn-codes');
-    Route::get('/colours/{id}', App\Livewire\Common\Index::class)->name('colours');
-    Route::get('/sizes/{id}', App\Livewire\Common\Index::class)->name('sizes');
-    Route::get('/banks/{id}', App\Livewire\Common\Index::class)->name('banks');
-    Route::get('/ledgers/{id}', App\Livewire\Common\Index::class)->name('ledgers');
-    Route::get('/transports/{id}', App\Livewire\Common\Index::class)->name('transports');
-    Route::get('/departments/{id}', App\Livewire\Common\Index::class)->name('departments');
-    Route::get('/dispatches/{id}', App\Livewire\Common\Index::class)->name('dispatches');
-    Route::get('/receipt-types/{id}', App\Livewire\Common\Index::class)->name('receipt-types');
+    Route::get('/cities', App\Livewire\Common\CityList::class)->name('cities');
+    Route::get('/states', App\Livewire\Common\StateList::class)->name('states');
+    Route::get('/pincodes', App\Livewire\Common\PincodeList::class)->name('pincodes');
+    Route::get('/countries', App\Livewire\Common\CountryList::class)->name('countries');
+    Route::get('/hsncodes', App\Livewire\Common\HsncodeList::class)->name('hsncodes');
+    Route::get('/categories', App\Livewire\Common\CategoryList::class)->name('categories');
+    Route::get('/transports', App\Livewire\Common\TransportList::class)->name('transports');
+    Route::get('/departments', App\Livewire\Common\DepartmentList::class)->name('departments');
+    Route::get('/ledgers', App\Livewire\Common\LedgerList::class)->name('ledgers');
+    Route::get('/sizes', App\Livewire\Common\SizeList::class)->name('sizes');
+    Route::get('/colours', App\Livewire\Common\ColourList::class)->name('colours');
+    Route::get('/despatches', App\Livewire\Common\DespatchList::class)->name('despatches');
+    Route::get('/banks', App\Livewire\Common\BankList::class)->name('banks');
+    Route::get('/receipttypes', App\Livewire\Common\ReceipttypeList::class)->name('receipttypes');
 
     Route::get('Factory', App\Livewire\Demo\Data\Factory\Index::class)->name('Factory');
 //    Route::get('productFactory', App\Livewire\Demo\Data\Product\Index::class)->name('productFactory');
