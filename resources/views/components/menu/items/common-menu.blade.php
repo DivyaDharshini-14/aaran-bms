8adde2cb3895d69706d803ffcce3926b7c912c1f
Rajesh18rj <rajeshkumar18rj@gmail.com>
2025-01-11 11:49:47 +0530

    #32 reworking on common menu

--- resources/views/components/menu/items/common-menu.blade.php
+++ resources/views/components/menu/items/common-menu.blade.php
@@ -1,18 +1,16 @@
-<x-menu.base.route-menuitem  href="{{route('cities')}}" :label="'City'"/>
+<x-menu.base.route-menuitem  href="{{route('cities')}}" :label="'Cities'"/>
 <x-menu.base.route-menuitem  href="{{route('states')}}" :label="'States'"/>
 <x-menu.base.route-menuitem  href="{{route('pin-codes')}}" :label="'Pin codes'"/>
 <x-menu.base.route-menuitem  href="{{route('countries')}}" :label="'Countries'"/>
 <x-menu.base.route-menuitem  href="{{route('hsn-codes')}}" :label="'Hsn codes'"/>
+<x-menu.base.route-menuitem  href="{{route('units')}}" :label="'Units'"/>
+<x-menu.base.route-menuitem  href="{{route('categories')}}" :label="'Categories'"/>
 <x-menu.base.route-menuitem  href="{{route('colours')}}" :label="'Colours'"/>
 <x-menu.base.route-menuitem  href="{{route('sizes')}}" :label="'Sizes'"/>
-<x-menu.base.route-menuitem  href="{{route('banks')}}" :label="'Banks'"/>
-<x-menu.base.route-menuitem  href="{{route('ledgers')}}" :label="'Ledgers'"/>
-<x-menu.base.route-menuitem  href="{{route('transports')}}" :label="'Transports'"/>
 <x-menu.base.route-menuitem  href="{{route('department')}}" :label="'Departments'"/>
-<x-menu.base.route-menuitem  href="{{route('dispatches')}}" :label="'Dispatches'"/>
+<x-menu.base.route-menuitem  href="{{route('transports')}}" :label="'Transports'"/>
+<x-menu.base.route-menuitem  href="{{route('banks')}}" :label="'Banks'"/>
 <x-menu.base.route-menuitem  href="{{route('receipt-types')}}" :label="'Receipt Types'"/>
-<x-menu.base.route-menuitem  href="{{route('receipt-types')}}" :label="'Product Type'"/>
-<x-menu.base.route-menuitem  href="{{route('receipt-types')}}" :label="'Units'"/>
-<x-menu.base.route-menuitem  href="{{route('receipt-types')}}" :label="'GST Percent'"/>
+<x-menu.base.route-menuitem  href="{{route('dispatches')}}" :label="'Dispatches'"/>
 <x-menu.base.li-menuitem :routes="'styles'" :label="'Styles'"/>
 
